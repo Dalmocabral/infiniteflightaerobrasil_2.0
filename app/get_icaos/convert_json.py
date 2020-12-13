6cecc9dd04cbf63cfce3c4c7b086e081bd8930de
dalmocabral <dalmo.santos.cabral@gmail.com>
2020-12-13 13:20:29 -0300

    commit

--- app/get_icaos/convert_json.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-data = 'https://raw.githubusercontent.com/InfiniteFlightAirportEditing/Navigation/master/Fixes.json'
-
-df = pd.read_json(data)
-
-df = df.drop_duplicates()
-
-df.to_excel(r'D:\DATAFILE.xlsx', index=None, header=True)
